0fcf8264f9fcca8476e0c18db8d6bc9837a7dc70
RayMoroney <72605559+RayMoroney@users.noreply.github.com>
2020-10-11 13:05:51 +0100

    Product Model and Storage

--- MyShop/MyShop.Core/Models/ProductModel.cs
+++ MyShop/MyShop.Core/Models/ProductModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,21 @@ namespace MyShop.Core.Models
 {
     public class ProductModel
     {
+        public string Id { get; set; }
+
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public ProductModel()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
     }
 }
