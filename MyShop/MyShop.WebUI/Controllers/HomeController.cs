97528fc866876ecc94b220e46c09df555dffb57d
RayMoroney <72605559+RayMoroney@users.noreply.github.com>
2020-10-25 14:59:19 +0000

    Added Product Listing.

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,33 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<ProductModel> context;
+        IRepository<ProductCategoryModel> productCategories;
+
+        public HomeController(IRepository<ProductModel> productContext, IRepository<ProductCategoryModel> productCategoriesContext)
+        {
+            context = productContext;
+            productCategories = productCategoriesContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<ProductModel> products = context.Collection().ToList();
+            return View(products);
         }
 
+        public ActionResult Details(string id)
+        {
+            ProductModel product = context.Find(id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
