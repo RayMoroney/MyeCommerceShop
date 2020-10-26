ac06ed20213706127e615e998665568eaf102d2d
RayMoroney <72605559+RayMoroney@users.noreply.github.com>
2020-10-26 22:19:06 +0000

    Created Mocks for Unit Tests.

--- MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
 
@@ -12,5 +15,20 @@ namespace MyShop.WebUI.Tests.Controllers
     [TestClass]
     public class HomeControllerTest
     {
+        [TestMethod]
+        public void IndexPageDoesReturnProducts()
+        {
+            IRepository<ProductModel> productContext = new Mocks.MockContext<ProductModel>();
+            IRepository<ProductCategoryModel> productCategoriesContext = new Mocks.MockContext<ProductCategoryModel>();
+
+            productContext.Insert(new ProductModel());
+
+            HomeController homeController = new HomeController(productContext, productCategoriesContext);
+
+            var result = homeController.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(1, viewModel.Products.Count());
+        }
     }
 }
